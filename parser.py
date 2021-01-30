f31fd83f552387ae78c61c5ea09ce926f47a0d44
Byron <47398200+byronxc@users.noreply.github.com>
2021-01-30 14:02:09 -0800

    added english to ASL grammar

---
+++ parser.py
@@ -0,0 +1,152 @@
+import azure.cognitiveservices.speech as speechsdk
+import stanfordnlp
+from operator import itemgetter, attrgetter, methodcaller
+
+nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos,lemma,depparse', treebank='en_ewt', use_gpu=False, pos_batch_size=3000) # Build the pipeline, specify part-of-speech processor's batch size
+
+def parse(text):
+
+  doc = nlp(text)
+
+  for sentence in doc.sentences:
+    translation = translate(sentence)
+    result = []
+    for word in translation[0]:
+      result.append((word['text'].lower(), word['lemma'].lower()))
+    print("\nResult: ", result, "\n")
+  return result
+
+def getLemmaSequence(meta):
+  tone = ""
+  translation = []
+  for word in meta:
+    if (word['text'].lower(), word['lemma'].lower()) not in (('is', 'be'), ('the', 'the'), ('of', 'the'), ('is', 'are'), ('by', 'by'), (',', ','), (';', ';'), (':'), (':')):
+      
+      if word['upos'] == 'PUNCT':
+        if word['lemma'] == "?":
+          tone = "?"
+        elif word['lemma'] == "!":
+          tone = "!"
+        else:
+          tone = ""
+        continue
+      
+      elif word['upos'] == 'SYM' or word['upos'] == 'X':
+        continue
+      
+      if word['upos'] == 'PART':
+        continue
+
+      elif word['upos'] == 'PROPN':
+        fingerSpell = []
+        for letter in word['text'].lower():
+          print(letter)
+          spell = {}
+          spell['text'] = letter
+          spell['lemma'] = letter
+        
+          fingerSpell.append(spell)
+        print(fingerSpell)
+        translation.extend(fingerSpell)
+        print(translation)
+
+      elif word['upos'] == 'NUM':
+        fingerSpell = []
+        for letter in word['text'].lower():
+          spell = {}
+         
+          pass
+        
+          fingerSpell.append(spell)
+
+      elif word['upos'] == 'CCONJ':
+        translation.append(word)
+      
+      elif word['upos'] == 'SCONJ':
+        if (word['text'].lower(), word['lemma'].lower() not in (('that', 'that'))):
+          translation.append(word)
+      
+      elif word['upos'] == 'INTJ':
+        translation.append(word)
+
+      elif word['upos']=='ADP':
+        pass
+
+      elif word['upos']=='DET':
+        pass
+
+      elif word['upos']=='ADJ':
+        translation.append(word)
+
+      elif word['upos'] == 'PRON' and word['dependency_relation'] not in ('nsubj'):
+        translation.append(word)
+
+      elif word['upos'] == 'NOUN':
+        translation.append(word)
+
+      elif word['upos']=='ADV':
+        translation.append(word)
+      
+      elif word['upos']=='AUX':
+        pass
+
+      elif word['upos']=='VERB':
+        translation.append(word)
+
+
+  return (translation, tone)
+def getMeta(sentence):
+
+  englishStruct = {}
+  aslStruct = {
+    'rootElements':[],
+    'UPOS': {
+      'ADJ':[], 'ADP':[], 'ADV':[], 'AUX':[], 'CCONJ':[], 'DET':[], 'INTJ':[], 'NOUN':[], 'NUM':[], 'PART':[], 'PRON':[], 'PROPN':[], 'PUNCT':[], 'SCONJ':[], 'SYM':[], 'VERB':[], 'X':[]
+    }
+  }
+  reordered = []
+
+  words = []
+  for token in sentence.tokens:
+
+    for word in token.words:
+      
+      print(word.index, word.governor, word.text, word.lemma, word.upos, word.dependency_relation) 
+  
+      j = len(words)
+      for i, w in enumerate(words):
+        if word.governor <= w['governor']:
+          continue
+        else:
+          j = i
+          break
+     
+      words.insert(j, wordToDictionary(word))
+
+  reordered = words
+
+
+  return reordered
+
+def wordToDictionary(word):
+  dictionary = {
+    'index': word.index,
+    'governor': word.governor,
+    'text': word.text.lower(),
+    'lemma': word.lemma.lower(),
+    'upos': word.upos,
+    'xpos': word.xpos,
+    'dependency_relation': word.dependency_relation,
+    'feats': word.dependency_relation,
+    'children': []
+  }
+  return dictionary
+
+def translate(parse):
+  meta = getMeta(parse)
+  translation = getLemmaSequence(meta)
+  return translation
+
+while True:
+  userInput = input("Enter a sentence: ")
+  parse(userInput)
